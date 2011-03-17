696a65d58f4d83dfb37eed3529481c3c6f11e7c2
Koji Ishimoto <ijok.ijok@gmail.com>
2011-03-17 19:17:37 +0900

    .
    Signed-off-by: Koji Ishimoto <ijok.ijok@gmail.com>

--- atnder_manager.user.js
+++ atnder_manager.user.js
@@ -1,11 +1,11 @@
-// ==UserSc/ ==UserScript==
+// ==UserScript==
 // @name         ATND Manager
 // @namespace    http://twitter.com/t32k
 // @description  Make Twitter List.
 // @version      0.1
 // @author       Koji Ishimoto
 // @include      http://atnd.org/users/*
-// @icon         https://github.com/t32k/ATND-Manager/raw/master/icon.png
+// @icon         https://github.com/t32k/ATND-Manager/raw/master/icon.png	
 // ==/UserScript==
 (function () {
   var s = document.createElement('script');
